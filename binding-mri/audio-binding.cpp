bb75ba21f40929cdf440ce82cdd9d3cf09586ddb
ZakatTeamI2P <zakatteam@proton.me>
2026-05-12 15:46:18 +0400

    fixes

--- binding-mri/audio-binding.cpp
+++ binding-mri/audio-binding.cpp
@@ -162,5 +162,5 @@ void audioBindingInit(){
 	_rb_define_module_function(module, "__reset__", audioReset);
 
 	INIT_AUD_PROP_BIND( BGM_Volume, "bgm_volume" );
-	INIT_AUD_PROP_BIND( SFX_Volume, "sfx_volume" );es
+	INIT_AUD_PROP_BIND( SFX_Volume, "sfx_volume" );
 }
