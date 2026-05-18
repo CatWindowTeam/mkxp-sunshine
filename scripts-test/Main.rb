5fd7991db2dae002e5f419a210084c72af420e0a
DepressedTWM <imtoolazyforcreatinggithubaccount@localhost>
2026-05-18 07:39:57 +0400

    Cleanup

--- scripts-test/Main.rb
+++
@@ -1,3 +0,0 @@
-begin
-  print("Oneshot: sunshine TEST xScripts.rxdata\nRuby version: #{RUBY_VERSION}\nSDL version: #{Sunshine::SDLVersion_major}.#{Sunshine::SDLVersion_minor}.#{Sunshine::SDLVersion_micro}")
-end
