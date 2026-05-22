6470bc6e4bdfdb3247e228c5bdaeb81cb8391525
DepressedTWM <imtoolazyforcreatinggithubaccount@localhost>
2026-05-22 10:13:57 +0400

    rpgscript.rb fix for riby 3

--- rpgscript.rb
+++ rpgscript.rb
@@ -43,7 +43,7 @@ list_path = File.join(scripts_dir, '_scripts.txt')
 
 if extract
   # Make sure the script directory exists
-  Dir.mkdir(scripts_dir) unless Dir.exists? scripts_dir
+  Dir.mkdir(scripts_dir) unless Dir.exist? scripts_dir
 
   # Keep track of names of scripts extracted so we can warn about duplicates
   names = Hash.new(0)
