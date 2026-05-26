db64eb7f0fa1feb60f0ab9d865fd7241d994154d
DepressedTWM <imlazyshit@arch.gay>
2026-05-26 22:39:23 +0400

    a

--- installer/installer.sh
+++ installer/installer.sh
@@ -5,16 +5,23 @@ read -rp "Enter your Oneshot 2016 installation path: " oneshot_path
 read -rp "Enter path for installation: " install_path
 
 mkdir -p $install_path
-cp -r "$oneshot_path/*" "$install_path/"
-cp -r "Sunshine/*" "$install_path/"
+cp -r $oneshot_path/* "$install_path/"
+cp -r Sunshine/* "$install_path/"
 
 echo """[Desktop Entry]
 Type=Application
 Name=Oneshot: Sunshine
 Comment=Oneshot mod
-Exec=$install_path/oneshot
+Exec=$install_path/oneshot.sh
 Icon=$install_path/icon.png
 Categories=Game;
 """ > $HOME/.local/share/applications/sunshine.desktop
+echo """
+#!/bin/bash
+LD_LIBRARY_PATH=$install_path\;$LD_LIBRARY_PATH $install_path/oneshot
+""" > $install_path/oneshot.sh
+chmod +x $install_path/oneshot.sh
+$install_path/oneshot
+LD_LIBRARY_PATH
 
 echo "Done!"
