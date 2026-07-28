893167533f6b8ce7c36bec26fc99bfb758e7172e
Issac332 <119879937+Issac8658@users.noreply.github.com>
2026-07-28 23:01:34 +0300

    meow :3

--- scripts/Data_Settings_Setters.rb
+++ scripts/Data_Settings_Setters.rb
@@ -70,19 +70,19 @@ module Settings
       end
 
       def SDL_HINT_INVALID_PARAM_CHECKS(value)
-      	if value
-			Sunshine.setSDLHint("SDL_HINT_INVALID_PARAM_CHECKS", "1")
-      	else
-      		Sunshine.setSDLHint("SDL_HINT_INVALID_PARAM_CHECKS", "2")
-		end
+        if value
+          Sunshine.setSDLHint("SDL_HINT_INVALID_PARAM_CHECKS", "1")
+        else
+          Sunshine.setSDLHint("SDL_HINT_INVALID_PARAM_CHECKS", "2")
+        end
       end
 
       def SDL_HINT_SHUTDOWN_DBUS_ON_QUIT(value)
-      	if value
-			Sunshine.setSDLHint("SDL_HINT_SHUTDOWN_DBUS_ON_QUIT", "1")
-      	else
-      		Sunshine.setSDLHint("SDL_HINT_SHUTDOWN_DBUS_ON_QUIT", "0")
-		end
+        if value
+          Sunshine.setSDLHint("SDL_HINT_SHUTDOWN_DBUS_ON_QUIT", "1")
+        else
+          Sunshine.setSDLHint("SDL_HINT_SHUTDOWN_DBUS_ON_QUIT", "0")
+        end
       end
     end
   end
