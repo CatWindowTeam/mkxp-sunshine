e44254f57cf09a6add84a6987c4a36a0d94bfe6c
CatWindow <catwindow@meow.org>
2026-09-06 11:10:44 +0300

    meow

--- binding-mri/audioplayback-binding.cpp
+++ binding-mri/audioplayback-binding.cpp
@@ -68,7 +68,7 @@ static VALUE rb_playbackLengthNormalized(VALUE self) {
 
 static VALUE rb_playbackAddTag(int argc, VALUE *argv, VALUE self) {
     char* c_str;
-    rb_get_args(argc, argv, c_str RB_ARG_END);
+    rb_get_args(argc, argv, "z", &c_str RB_ARG_END);
     PLAYBACK
     pb->addTag(c_str);
 
@@ -77,7 +77,7 @@ static VALUE rb_playbackAddTag(int argc, VALUE *argv, VALUE self) {
 
 static VALUE rb_playbackRemoveTag(int argc, VALUE *argv, VALUE self) {
     char* c_str;
-    rb_get_args(argc, argv, c_str RB_ARG_END);
+    rb_get_args(argc, argv, "z", &c_str RB_ARG_END);
     PLAYBACK
     pb->removeTag(c_str);
 
