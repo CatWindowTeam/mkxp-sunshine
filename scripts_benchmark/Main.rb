9658b8879837a6ba99bb11089db747ec97ef47da
DepressedTWM <imlazyshit@arch.gay>
2026-09-07 17:29:04 +0400

    a

--- scripts_benchmark/Main.rb
+++ scripts_benchmark/Main.rb
@@ -1,7 +1,45 @@
-c = 0
-Graphics.frame_rate = 1000
-while c < 1000
-	c = c + 1
-	Font.default_size = c
-	Graphics.update
+begin
+  RPG::Mod.exec_hooks("hooks/Main/start", binding)
+  $console = Graphics.fullscreen
+  Graphics.frame_rate = 60
+  Font.default_size = 20
+  #debug shit 
+  Input.set_led(255, 150, 30)
+  # Registering parameters
+  Settings.reset!
+  # Load persistent data
+  Persistent.load
+
+  # Prepare for transition
+  Graphics.freeze
+  # Make scene object (title screen)
+  $scene = Scene_Title.new
+  Oneshot.allow_exit false
+  Oneshot.exiting false
+
+  finish_time = Time.now + 5 * 60
+  while CTime.now < finish_time
+    puts "TEST PUTS"
+    Logger.Debug "TEST LOGS DEBUG"
+    Logger.Info "TEST LOGS INFO"
+    Logger.Warn "TEST LOGS WARN"
+    Logger.Error "TEST LOGS ERROR"
+  end
+  save unless $game_switches[99] || ($game_system.map_interpreter.running? || !$scene.is_a?(Scene_Map))
+rescue Errno::ENOENT => e
+  Sunshine.SetCrashScreenData "#{e.message}"
+rescue StandardError => e
+  Sunshine.SetCrashScreenData "#{e.class}: #{e.message}"
+  e.backtrace.each do |line|
+    puts line
+  end
+ensure
+  # Fade out
+  Oneshot.exiting true
+  Graphics.transition(20)
+  if Journal.active?
+    Journal.set 'default'
+  end
+  Oneshot.allow_exit true
+  Wallpaper.reset
 end
--- src/main.cpp
+++ src/main.cpp
@@ -146,7 +146,8 @@ static void setupWindowIcon(const Config &conf, SDL_Window *win){
 
 int main(int argc, char *argv[]){
 	std::set_terminate(&terminate_stacktrace);
-        startTime = boost::chrono::high_resolution_clock::now();
+    startTime = boost::chrono::high_resolution_clock::now();
+    SDL_SetHint(SDL_HINT_INVALID_PARAM_CHECKS, "1");
 	loadLanguageMetadata(); //there will be a segfault on fclose if I don't move it here
 	SDL_SetHint(SDL_HINT_VIDEO_MINIMIZE_ON_FOCUS_LOSS, "0");
 	SDL_SetAppMetadata("Oneshot: Sunshine", VERSION_STRING, "meow.catwindowteam.sunshine");
