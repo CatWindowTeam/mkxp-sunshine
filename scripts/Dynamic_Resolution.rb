065372d1a4adb848ebe6efbb389563ae1ead84ac
CatWindow <catwindow@meow.org>
2026-09-07 22:45:51 +0300

    meow 3:

--- scripts/Dynamic_Resolution.rb
+++ scripts/Dynamic_Resolution.rb
@@ -1,13 +1,13 @@
 module Graphics
-  def self.adapted_file(path, extention = ".png")
+  def self.adapted_file(path, extension = ".png")
     if Settings[:replace_all_niko]
       path = "Graphics/Faces/niko"
     end
     file_tag = RESOLUTIONS[Settings[:resolution] || 999]&.[](:file_tag) || ""
-    if PhysFS.exist?(path + file_tag + extention)
+    if PhysFS.exist?(path + file_tag + extension)
       return path + file_tag
     else
-      if File.exist?(path + file_tag + extention)
+      if File.exist?(path + file_tag + extension)
         return path + file_tag
       end
     end
