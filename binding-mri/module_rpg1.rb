2e07f409b3e4c6d8ca0d072d44231451f2843a0f
DepressedTWM <imlazyshit@arch.gay>
2026-09-10 22:02:07 +0400

    a

--- binding-mri/module_rpg1.rb
+++ binding-mri/module_rpg1.rb
@@ -1457,6 +1457,25 @@ module RPG
       attr_accessor :equip
     end
 
+    class TestBattler
+      def initialize
+        @actor_id = 1
+        @level = 1
+        @weapon_id = 0
+        @armor1_id = 0
+        @armor2_id = 0
+        @armor3_id = 0
+        @armor4_id = 0
+      end
+      attr_accessor :actor_id
+      attr_accessor :level
+      attr_accessor :weapon_id
+      attr_accessor :armor1_id
+      attr_accessor :armor2_id
+      attr_accessor :armor3_id
+      attr_accessor :armor4_id
+    end
+
     def initialize
       @magic_number = 0
       @party_members = [1]
