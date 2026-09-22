a3c2468c6970a9b5cea17516f5acca0a8b08e2da
DepressedTWM <imlazyshit@arch.gay>
2026-09-22 15:49:48 +0400

    Haiku build script

---
+++ make-oneshot-haiku.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+cmake . -B build/ "$@"
+cd build
+make -j$(nproc)
+cd ..
+
+mkdir -p build/bandle
+mkdir -p build/bandle
+mkdir -p build/bandle/Data
+
+ruby rpgscript.rb scripts/ build/bandle/
+cp build/oneshot build/bandle/
+
+# Copy libraries.
+mkdir -p libs
+ldd build/oneshot | ruby libraries.rb
+cp libs/* build/bandle
+cp build/oneshot build/bandle
+
+cp -r ../SunshineAssets/* build/bandle
+cp sunshine.conf build/bandle
+zip -X -9 -x ".git/*" "*/.git/*" -r build/OneshotSunshine_Linux.zip build/bandle/*
+
+rm -rf libs
