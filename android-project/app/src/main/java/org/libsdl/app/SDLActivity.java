e3d686cc3a40f48328d4dfe1a2d8a99dc0c62a37
DepressedTWM <imlazyshit@arch.gay>
2026-09-24 01:12:48 +0400

    a

--- android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -319,10 +319,6 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
             "SDL3_mixer",
             "SDL3_net",
             "SDL3_ttf",
-            "ruby",
-            "physfs",
-            "pixman-1",
-            "z",
             "main"
         };
     }
